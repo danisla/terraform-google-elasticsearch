c0f4b3bca7302d6ad7417322648f485b6613da47
Dan Isla <disla@google.com>
2018-08-20 23:03:35 -0700

    multi os terraform-install.sh

--- examples/terraform-install.sh
+++ examples/terraform-install.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 function terraform-install() {
-  OS=$(uname -s)
   [[ -f ${HOME}/bin/terraform ]] && echo "`${HOME}/bin/terraform version` already installed at ${HOME}/bin/terraform" && return 0
-  LATEST_URL=$(curl -sL https://releases.hashicorp.com/terraform/index.json | jq -r '.versions[].builds[].url' | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | egrep -v 'rc|beta' | egrep "${OS,,}.*amd64" |tail -1)
+  LATEST_URL=$(curl -sL https://releases.hashicorp.com/terraform/index.json | jq -r '.versions[].builds[].url' | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | egrep -v 'rc|beta' | egrep "$(uname | tr '[:upper:]' '[:lower:]').*amd64" |tail -1)
   curl ${LATEST_URL} > /tmp/terraform.zip
   mkdir -p ${HOME}/bin
   (cd ${HOME}/bin && unzip /tmp/terraform.zip)
