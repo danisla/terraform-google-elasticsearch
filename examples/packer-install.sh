48a2088eb5e9aaf170c0f213ab49d793630f0064
Dan Isla <disla@google.com>
2018-10-24 13:38:59 +0000

    fix to terraform-install.sh and packer-install.sh

--- examples/packer-install.sh
+++ examples/packer-install.sh
@@ -3,7 +3,7 @@
 function packer-install() {
   OS=$(uname -s)
   [[ -f ${HOME}/bin/packer ]] && echo "`${HOME}/bin/packer version` already installed at ${HOME}/bin/packer" && return 0
-  LATEST_URL=$(curl -sL https://releases.hashicorp.com/packer/index.json | jq -r '.versions[].builds[].url' | sort -n | egrep -v 'rc|beta' | egrep "${OS,,}.*amd64" |tail -1)
+  LATEST_URL=$(curl -sL https://releases.hashicorp.com/packer/index.json | jq -r '.versions[].builds[].url' | sort -n | egrep -v 'rc|alpha|beta' | egrep "${OS,,}.*amd64" |tail -1)
   curl ${LATEST_URL} > /tmp/packer.zip
   mkdir -p ${HOME}/bin
   (cd ${HOME}/bin && unzip /tmp/packer.zip)
