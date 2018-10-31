5bf3d17d297e634ede1e8a5de695eda03cea28e2
Dan Isla <disla@google.com>
2018-10-31 13:37:21 -0700

    update to install scripts

--- examples/packer-install.sh
+++ examples/packer-install.sh
@@ -3,7 +3,8 @@
 function packer-install() {
   OS=$(uname -s)
   [[ -f ${HOME}/bin/packer ]] && echo "`${HOME}/bin/packer version` already installed at ${HOME}/bin/packer" && return 0
-  LATEST_URL=$(curl -sL https://releases.hashicorp.com/packer/index.json | jq -r '.versions[].builds[].url' | sort -n | egrep -v 'rc|alpha|beta' | egrep "${OS,,}.*amd64" |tail -1)
+  LATEST_URL=$(curl -sL https://releases.hashicorp.com/packer/index.json | jq -r '.versions[].version' | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | egrep -v 'alpha|beta|rc' | tail -1)
+  LATEST_URL="https://releases.hashicorp.com/packer/${LATEST_VERSION}/packer_${LATEST_VERSION}_${OS}_amd64.zip"
   curl ${LATEST_URL} > /tmp/packer.zip
   mkdir -p ${HOME}/bin
   (cd ${HOME}/bin && unzip /tmp/packer.zip)
